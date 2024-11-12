da88380ebfeceae73081c24d52e8abc042b8bcb8
alexseals19 <alexseals19@gmail.com>
2024-11-12 10:31:46 -0600

    Created empty project.

---
+++ RecipesProject/RecipesProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesProjectApp.swift
+//  RecipesProject
+//
+//  Created by Alex Seals on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipesProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
